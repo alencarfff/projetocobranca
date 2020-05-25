62e53b9280f0d775dd94dd39aa5aacc0b124e278
Madrigar <tmadrigar>
2020-05-25 11:53:44 -0300

    Semana 11 - Validações

--- src/main/java/edu/br/unifcv/projetocobranca/controller/TituloController.java
+++ src/main/java/edu/br/unifcv/projetocobranca/controller/TituloController.java
@@ -3,6 +3,8 @@ package edu.br.unifcv.projetocobranca.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,16 +20,22 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject(new Titulo());
+		return mv;
 	}
 	
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-		titulos.save(titulo);
-		
+	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		if(errors.hasErrors()) {
+			return mv;
+		}
+		
+		titulos.save(titulo);
+			
 		mv.addObject("mensagem", "Titulo Salvo com Sucesso!");
 		return mv;
 		
